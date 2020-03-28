20b6913db2c49550ad4f3b3a62fda165ddaa229f
Kim, SoYeon <smfqha26@gmail.com>
2020-03-29 02:53:53 +0900

    2주차 과제

---
+++ hw/pages.js
@@ -0,0 +1,3 @@
+let studentCount=31, pageSize =10;
+let number = Math.ceil(studentCount/pageSize);
+console.log(number);
---
+++ hw/random.js
@@ -0,0 +1,5 @@
+let Max=20, Min=10;
+for(let i=0; i<30; ++i){
+    let num=Math.floor(Math.random()*(Max-Min+1)+Min);
+    console.log(num);
+}
