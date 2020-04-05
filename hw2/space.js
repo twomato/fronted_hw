76d9807552dd5750156cb7b302e1e38f2603a2f7
Kim, SoYeon <smfqha26@gmail.com>
2020-04-05 23:15:55 +0900

    3주차

---
+++ hw2/space.js
@@ -0,0 +1,3 @@
+let s="When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom, let it be.";
+
+console.log(s.replace(/ /g, ""));
---
+++ hw2/vowel.js
@@ -0,0 +1,15 @@
+let s="When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom, let it be.";
+
+for(let i=0; i<s.length; i++){
+    if(s[i]=="a")
+    console.log(i);
+    if(s[i]=="e")
+    console.log(i);
+    if(s[i]=="i")
+    console.log(i);
+    if(s[i]=="o")
+    console.log(i);
+    if(s[i]=="u")
+    console.log(i);
+}
+
